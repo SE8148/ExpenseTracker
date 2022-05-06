3e63d0f9295a70f9641f6c4c612900616a26097b
Liam <liam.seeney@outlook.com>
2022-05-06 23:17:03 +0100

    Adds starter models and UI

---
+++ ExpenseTrackerData/Models/IncomingExpense.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseTrackerData.Models
+{
+    public class IncomingExpense
+    {
+        public string CustomerName { get; set; }
+        public int TransactionID { get; set; }
+        public decimal IncomingAmount { get; set; }
+        public DateOnly IncomingDate { get; set; }
+        public string IncomingDescription { get; set; }
+    }
+}
---
+++ ExpenseTrackerData/Models/OutgoingExpense.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseTrackerData.Models
+{
+    public class OutgoingExpense
+    {
+        public string OutgoingRecipient { get; set; }
+        public decimal OutgoingAmount { get; set; }
+        public string OutgoingDescription { get; set; }
+        public DateOnly ExpenseDate { get; set; }
+    }
+}
